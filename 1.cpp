a3e3c1dc99d56aab4405956c321fec38301e7e83
vishvamitra4 <vishvamitrakumarsingh@gmail.com>
2025-06-15 09:47:34 +0530

    1

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        int profit = 0;
+        int mini = prices[0];
+        for(int i = 1; i < n; i++) {
+            profit = max(profit , prices[i] - mini);
+            mini = min(mini , prices[i]);
+        };
+
+        return profit;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
+
